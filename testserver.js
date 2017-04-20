5f425406c33b7f04f90e09a988e09815705e6352
ononop <ononop@tcd.ie>
2017-04-20 16:19:29 +0100

    change redirect uri

--- testserver.js
+++ testserver.js
@@ -55,7 +55,7 @@ app.get('/login', function(req, res) {
     }));
 });
 
-app.get('/callback', function(req, res) {
+app.get('/tobyonono.github.io', function(req, res) {
 
   // your application requests refresh and access tokens
   // after checking the state parameter
