6dcb56feb910096d2795e53429c82e59a6907a5f
Peter-Alexander Tobechukwutoby Onono <ononop@ITSERV.SCSS.TCD.IE>
2017-06-26 20:52:57 +0100

    'click-to-get-user-and-click-to-save-song'

--- testserver.js
+++ testserver.js
@@ -55,7 +55,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-read-currently-playing';
+  var scope = 'user-read-private user-read-email user-read-currently-playing user-library-modify';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
